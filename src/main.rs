0443ddabfeb4dab1c1334ffec3e04ffa9cacd34f
jam1garner <8260240+jam1garner@users.noreply.github.com>
2020-10-19 13:09:48 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Args {
+    image: PathBuf,
+    out: PathBuf,
+
+    #[structopt(long)]
+    width: Option<u32>,
+
+    #[structopt(long)]
+    height: Option<u32>,
+
+    #[structopt(short, long)]
+    name: Option<String>,
+}
+
+fn main() {
+    let args = Args::from_args();
+
+    let image = match (args.width, args.height) {
+        (Some(width), Some(height)) => image::DynamicImage::ImageRgba8(image::RgbaImage::new(width, height)),
+        _ => image::open(args.image).unwrap()
+    };
+
+    bntx::BntxFile::from_image(image, args.name.as_deref().unwrap_or("file"))
+        .save(args.out)
+        .unwrap()
+}
